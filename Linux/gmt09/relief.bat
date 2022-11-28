9367af320a683b72f96713c99bc0cf6c3823ac11
NiKoJJ <zoexjj216@gmail.com>
2022-11-28 23:38:19 +0800

    add new region

--- Linux/gmt09/relief.bat
+++ Linux/gmt09/relief.bat
@@ -1,3 +1,9 @@
 gmt begin global_relief png,pdf
+
+gmt figure fg1
 gmt grdimage @earth_relief_15m -JH180/10c
+
+gmt figure fg2
+gmt grdimage @earth_relief_30s -JM15c -R118/125/20/26 -Baf -BWsen
+
 gmt end show
